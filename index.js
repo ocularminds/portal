c1e375b6b4b754b2ddb1681de31e497d6a22eb3f
Festus B. Jejelowo <mail.festus@gmail.com>
2020-05-19 01:41:30 +0100

    added authentication with auth0

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 const express = require('express');
 const path = require('path');
-const {auth} = require('express-openid-connect');
+const expressSession = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const authRouter  = require('./auth');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 1427;
-const config = {
-    required: false,
-    auth0Logout: true,
-    appSession: {
-      secret: 'a long, randomly-generated string stored in env'
-    },
-    baseURL: 'http://localhost:3000',
-    clientID: 'G5zLpAejLAVAWg5VxC0yxJ0rQREvKs00',
-    issuerBaseURL: 'https://dev-auo9rt3x.auth0.com'
-  };
+const session = {
+  secret: 'LoxodontaElephasMammuthusPalaeoloxodonPrimelephas',
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+if (app.get('env') === 'production') {
+  session.cookie.secure = true;
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(auth(config));
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+app.use('/', authRouter);
 
 app.get('/', (req, res) => {
   //res.status(200).send("WHATABYT: Food For Devs");
